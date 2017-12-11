218e46082bffcaed59e1946bf86411efa84bbb4d
Savand <savka.and@gmail.com>
2017-12-11 10:57:19 +0200

    Changes regarding convention in thee property files

--- src/main/java/org/savand/budget_manager/BudgetManagerApplication.java
+++ src/main/java/org/savand/budget_manager/BudgetManagerApplication.java
@@ -2,8 +2,10 @@ package org.savand.budget_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BudgetManagerApplication {
 
 	public static void main(String[] args) {
